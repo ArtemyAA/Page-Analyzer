6c383ba46cb60959b673bdd1141cddfee5541773
Artemy Ablaev <artemyablaev.gmail.com>
2024-04-04 20:22:37 +0300

    fix wrong url check

--- page_analyzer/validator.py
+++ page_analyzer/validator.py
@@ -3,7 +3,7 @@ from urllib.parse import urlparse
 from page_analyzer.database_helper import already_exists
 import requests
 from bs4 import BeautifulSoup
-
+from requests.exceptions import RequestException
 
 def parse(url):
     parsed = urlparse(url)
@@ -25,14 +25,20 @@ def validate(url):
 
 
 def get_url_info(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    response.encoding = 'utf-8'
-    status_code = response.status_code
-    html_content = response.text
-    soup = BeautifulSoup(html_content, 'html.parser')
-    h1 = soup.h1.get_text() if soup.h1 else ''
-    title = soup.title.string if soup.title else ''
-    description_tag = soup.find('meta', attrs={'name': 'description'})
-    description = description_tag['content'] if description_tag else ''
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Проверяем статус ответа
+        response.encoding = 'utf-8'
+        status_code = response.status_code
+        html_content = response.text
+        soup = BeautifulSoup(html_content, 'html.parser')
+        h1 = soup.h1.get_text() if soup.h1 else ''
+        title = soup.title.string if soup.title else ''
+        description_tag = soup.find('meta', attrs={'name': 'description'})
+        description = description_tag['content'] if description_tag else ''
+    except RequestException:
+        status_code = 0
+        h1 = ''
+        title = ''
+        description = ''
     return status_code, h1, title, description
