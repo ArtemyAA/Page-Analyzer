27428db7b5a1ab29521f2900e8cb019b1f66583f
Artemy Ablaev <artemyablaev.gmail.com>
2024-04-17 15:27:20 +0300

    own fixes

--- page_analyzer/database_helper.py
+++ page_analyzer/database_helper.py
@@ -8,14 +8,26 @@ load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 
+def execute_query(query, params=None, fetchall=True):
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor(cursor_factory=DictCursor) as curs:
+            curs.execute(query, params)
+            if fetchall:
+                return curs.fetchall()
+            else:
+                return curs.fetchone()
+
+
 def add_url_to_db(new_url):
     created_at = datetime.datetime.now()
     with psycopg2.connect(DATABASE_URL) as conn:
         with conn.cursor(cursor_factory=DictCursor) as curs:
             curs.execute(
-                '''INSERT INTO urls (name, created_at)
+                '''
+                INSERT INTO urls (name, created_at)
                 VALUES (%s, %s)
-                RETURNING id;''',
+                RETURNING id;
+                ''',
                 (new_url['name'], created_at))
             inserted_id = curs.fetchone()['id']
         conn.commit()
@@ -23,36 +35,28 @@ def add_url_to_db(new_url):
 
 
 def get_url_by_id(id):
-    with psycopg2.connect(DATABASE_URL) as conn:
-        with conn.cursor(cursor_factory=DictCursor) as curs:
-            curs.execute(
-                'SELECT id, name, created_at FROM urls WHERE id=(%s);', (id,))
-            url = curs.fetchone()
-            return url
+    query = 'SELECT id, name, created_at FROM urls WHERE id=(%s);'
+    return execute_query(query, (id,), False)
 
 
 def get_url_by_name(name):
-    with psycopg2.connect(DATABASE_URL) as conn:
-        with conn.cursor(cursor_factory=DictCursor) as curs:
-            curs.execute(
-                '''SELECT id, name, created_at
-                FROM urls WHERE name=(%s);''', (name,))
-            url = curs.fetchone()
-            return url
+    query = '''
+            SELECT id, name, created_at
+            FROM urls
+            WHERE name=(%s);
+            '''
+    return execute_query(query, (name,), False)
 
 
 def get_urls_list():
-    with psycopg2.connect(DATABASE_URL) as conn:
-        with conn.cursor(cursor_factory=DictCursor) as curs:
-            curs.execute('''
-                        SELECT DISTINCT ON (urls.id)
-                        urls.id, urls.name, c.created_at, c.status_code
-                        FROM urls LEFT JOIN url_checks as c
-                        ON urls.id=c.url_id
-                        ORDER BY urls.id DESC, c.created_at DESC;
-                        ''')
-            urls_list = curs.fetchall()
-            return urls_list
+    query = '''
+            SELECT DISTINCT ON (urls.id)
+            urls.id, urls.name, c.created_at, c.status_code
+            FROM urls LEFT JOIN url_checks as c
+            ON urls.id=c.url_id
+            ORDER BY urls.id DESC, c.created_at DESC;
+            '''
+    return execute_query(query)
 
 
 def add_check(url_id, status_code, h1,
@@ -61,17 +65,20 @@ def add_check(url_id, status_code, h1,
     with psycopg2.connect(DATABASE_URL) as conn:
         with conn.cursor(cursor_factory=DictCursor) as curs:
             curs.execute(
-                '''INSERT INTO url_checks
+                '''
+                INSERT INTO url_checks
                 (url_id, status_code, h1, title, description, created_at)
-                VALUES (%s, %s, %s, %s, %s, %s);''',
+                VALUES (%s, %s, %s, %s, %s, %s);
+                ''',
                 (url_id, status_code, h1, title, description, created_at))
         conn.commit()
 
 
 def get_check_list(id):
-    with psycopg2.connect(DATABASE_URL) as conn:
-        with conn.cursor(cursor_factory=DictCursor) as curs:
-            curs.execute('''SELECT * FROM url_checks
-                        WHERE url_id=(%s) ORDER BY id DESC;''', (id,))
-            url_checks = curs.fetchall()
-            return url_checks
+    query = '''
+            SELECT *
+            FROM url_checks
+            WHERE url_id=(%s)
+            ORDER BY id DESC;
+            '''
+    return execute_query(query, (id,))
